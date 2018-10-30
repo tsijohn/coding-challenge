b3bcd4f792726aaaa59bdbd284a909020611f9b0
John <utjohnkkim@gmail.com>
2018-10-29 22:44:12 -0500

    On post delete, all of its related comments are deleted.

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "post delete also deletes all the comments" do
+    post = posts(:test_post_with_comment)
+    post.destroy
+    assert_equal 0, post.comments.count
+  end
 end
