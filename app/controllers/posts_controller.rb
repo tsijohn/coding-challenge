d18c63a017a13641d3dda064f30583f8650acb4a
John <utjohnkkim@gmail.com>
2018-10-29 22:09:40 -0500

    Basic posts controller create action, view, and test

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,13 +6,26 @@ class PostsController < ApplicationController
 
   def new
     # Return view to create a new Post
+    @post = Post.new
   end
 
   def create
+    @post = Post.create(post_params)
     # Add a new `Post` to the database
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
   end
 
   def destroy
     # Remove a `Post` from the database
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -6,4 +6,22 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     #testing index action
   end 
+
+  test "POST #create creates and returns new post" do
+    assert_difference "Post.count" do
+      post posts_path, params: create_post_params
+    end
+    assert_response :redirect
+  end
+
+  private
+
+  def create_post_params
+    {
+      post: {
+        title: "Test Post",
+        body: "Doing a create action test"
+      }
+    }
+  end
 end
