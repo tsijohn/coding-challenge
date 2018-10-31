ef821aa8ddb5cb18e67630a6dd3e02053f844953
John <utjohnkkim@gmail.com>
2018-10-30 23:20:54 -0500

    Move post length in minutes method from post model to post helper

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,14 @@
 module PostsHelper
+  def post_length_in_minutes(post)
+    if post.body.blank?
+      min = 0 
+    else 
+      min = post.body.split(" ").size / 200
+    end
+    if min == 0
+      'less than a minute read'
+    else
+      "#{min} min read"
+    end
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,17 +3,4 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   validates :title, :body, presence: true
 
-  def post_length_in_minutes
-    if self.body.blank?
-      min = 0 
-    else 
-      min = self.body.split(" ").size / 200
-    end
-    if min == 0
-      'less than a minute read'
-    else
-      "#{min} min read"
-    end
-  end
-
 end
