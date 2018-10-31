085d99343ecc4915e971c2279d23c71b719e6097
John <utjohnkkim@gmail.com>
2018-10-30 22:14:58 -0500

    Require post title and body on new posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   searchkick
   has_many :comments, dependent: :destroy
+  validates :title, :body, presence: true
 
   def post_length_in_minutes
     if self.body.blank?
