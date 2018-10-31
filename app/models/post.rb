93ca6de481d687fa4e04e01ec9aace6b5b9b3dcd
John <utjohnkkim@gmail.com>
2018-10-30 22:07:07 -0500

    Show post time to read and date on index page.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,18 @@
 class Post < ApplicationRecord
   searchkick
   has_many :comments, dependent: :destroy
+
+  def post_length_in_minutes
+    if self.body.blank?
+      min = 0 
+    else 
+      min = self.body.split(" ").size / 200
+    end
+    if min == 0
+      'less than a minute read'
+    else
+      "#{min} min read"
+    end
+  end
+
 end
